71fb14975339fd6f79e5abcb1a76e2ab6cb8c7dc
mete5050 <33295066+mete5050@users.noreply.github.com>
2018-05-06 13:45:37 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,7 @@
+#Bu program grafksel olarak gözlemlenebilir eğer raminiz 4gb uzeri değil ise en fazla 30 yazmanızı onreriyorum
+import os
+x = 0
+x = input("Site Adresi : ")
+i = int(input("Kaç kere grilsin : ")
+while x<=i:
+    os.system(x)
